afd27382dce319c3e9f7c80723d435630c7b6cfc
Nadia Cuadros <november138@hotmail.com>
2017-05-02 18:25:19 -0500

    dandole estilo a la sección de videos

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,15 @@ var email = document.getElementById("email");
 var city = document.getElementById("city");
 var formu = document.getElementById("formu");
  login.addEventListener("onclick", function(){
-   if(formu.style.display===none){
-     formu.style.display==block;
+   if(formu.display===none){
+     formu.display==block;
    }
  });
  var correcto = /^[A-Z]{1}[a-z]*/;
- function validar(){
-   if((!correcto.test(name))|| (!correcto.test(city))){
-     
 
+ function validar(){
+   if((!correcto.test(name))|| (!correcto.test(city))){ 
+     var error = document.getElementsByClassName('className')[1];
+     error.display==block;
    }
  }
